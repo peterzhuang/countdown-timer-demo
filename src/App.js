e4f2b939a957d0e076b1198474c9216df820c7ca
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2022-06-13 19:26:39 -0400

    Countdown Timer Demo app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CountdownTimer from './Components/CountdownTimer/CountdownTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <CountdownTimer countdownTimestampMs={1655333320175}/>
+    </div>
+  );
+}
+
+export default App;
